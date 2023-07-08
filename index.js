2586a9e7aa07f5cb90bd492ba35b090a7ed86291
Jojhan Silva <silvajojhan@gmail.com>
2023-07-07 20:13:58 -0500

    creates seperate file, uses shape classes

--- index.js
+++ index.js
@@ -34,16 +34,48 @@ const questions = [
     }
 ,];
 
-function generateLogo(answers){
-    const {characters, textColor, shape, shapeColor} = answers;
-}
-
-function init() {
-    inquirer.prompt(questions).then((answers) => {
-        generateLogo(answers)
-
+function generateLogo(answers) {
+    const { characters, textColor, shape, shapeColor } = answers;
+    let shapeInstance;
+  
+    switch (shape) {
+      case 'circle':
+        shapeInstance = new Circle();
+        break;
+      case 'square':
+        shapeInstance = new Square();
+        break;
+      case 'triangle':
+        shapeInstance = new Triangle();
+        break;
+      default:
+        console.log('Invalid shape');
+        return;
+    }
+  
+    shapeInstance.setColor(shapeColor);
+  
+    const svgContent = `
+      <svg xmlns="http://www.w3.org/2000/svg" width="200" height="200">
+        ${shapeInstance.render()}
+        <text x="50%" y="50%" text-anchor="middle" dominant-baseline="middle" fill="${textColor}" font-size="50">${characters}</text>
+      </svg>
+    `;
+  
+    fs.writeFile('logo.svg', svgContent, (err) => {
+      if (err) {
+        console.error('Error writing SVG file:', err);
+      } else {
+        console.log('SVG logo generated successfully!');
+      }
     });
   }
+  
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+      generateLogo(answers);
+    });
+  }
+  
 // Function call to initialize app
 init();
-
